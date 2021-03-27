5ca69d573becce287c693ade8d672592b18bad3e
骆思杰 <luo_sj@163.com>
2021-03-27 16:31:54 +0800

    update

---
+++ src/test/app.ts
@@ -0,0 +1,82 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+
+import { Engine, Scene } from "@babylonjs/core";
+// App class is our entire game application
+class App {
+    // General Entire Application
+    private _scene: Scene;
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
+
+    //Game State Related
+    public assets;
+
+    constructor() {
+        this._canvas = this._createCanvas();
+
+        // 初始化 babylon 场景(scene) and 引擎(engin)
+        this._init();
+    }
+
+    private async _init(): Promise<void> {
+        this._engine = new Engine(this._canvas, true);
+        this._scene = new Scene(this._engine);
+
+        //**for development: make inspector visible/invisible
+        window.addEventListener("keydown", (ev) => {
+            //Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if (this._scene.debugLayer.isVisible()) {
+                    this._scene.debugLayer.hide();
+                } else {
+                    this._scene.debugLayer.show();
+                }
+            }
+        });
+
+        //MAIN render loop & state machine
+        await this._main();
+    }
+
+    private async _main(): Promise<void> {
+        // Register a render loop to repeatedly render the scene
+        this._engine.runRenderLoop(() => {
+            this._scene.render()
+        });
+
+        //resize if the screen is resized/rotated
+        window.addEventListener('resize', () => {
+            this._engine.resize();
+        });
+    }
+
+    //set up the canvas
+    private _createCanvas(): HTMLCanvasElement {
+
+        //Commented out for development
+        document.documentElement.style["overflow"] = "hidden";
+        document.documentElement.style.overflow = "hidden";
+        document.documentElement.style.width = "100%";
+        document.documentElement.style.height = "100%";
+        document.documentElement.style.margin = "0";
+        document.documentElement.style.padding = "0";
+        document.body.style.overflow = "hidden";
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
+        document.body.style.margin = "0";
+        document.body.style.padding = "0";
+
+        //create the canvas html element and attach it to the webpage
+        this._canvas = document.createElement("canvas");
+        this._canvas.style.width = "100%";
+        this._canvas.style.height = "100%";
+        this._canvas.id = "gameCanvas";
+        document.body.appendChild(this._canvas);
+
+        return this._canvas;
+    }
+
+}
+new App();
