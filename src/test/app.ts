8177fb459d6732c12295407ae51999932df9c78a
骆思杰 <luo_sj@163.com>
2021-04-02 00:16:12 +0800

    update

--- src/test/app.ts
+++ src/test/app.ts
@@ -2,7 +2,7 @@
 // import '@babylonjs/inspector'
 import '@babylonjs/loaders/glTF'
 
-import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, Matrix, Color4, ParticleSystem, CannonJSPlugin, ParticleHelper, SolidParticle, SolidParticleSystem, Scalar, Mesh, Color3, StandardMaterial } from '@babylonjs/core'
+import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, SceneLoader, Matrix, Color4, ParticleSystem, CannonJSPlugin, ParticleHelper, SolidParticle, SolidParticleSystem, Scalar, Mesh, Color3, StandardMaterial, Camera } from '@babylonjs/core'
 // App class is our entire game application
 class App {
     // General Entire Application
@@ -14,21 +14,28 @@ class App {
     private plane: any
     private particles: Array<Mesh>
 
+    private pointerPos: any
+    private windowHalfX: number
+    private windowHalfY: number
+
     //Game State Related
     public assets
 
     constructor() {
-        this.canvas = this.createCanvas()
-        // 初始化 babylon 场景(scene) and 引擎(engin)
         this.init()
     }
 
+    // 初始化 babylon 场景(scene) and 引擎(engin)
     private async init(): Promise<void> {
+        this.pointerPos = { x: 0, y: 0}
+
+        this.canvas = this.createCanvas()
         this.engine = new Engine(this.canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false})
+
         this.scene = new Scene(this.engine)
         this.scene.clearColor = new Color4(229 / 255, 221 / 255, 190 / 255, 1)
 
-        this.camera = new FreeCamera('free-camera', new Vector3(0, 0, -25), this.scene)
+        this.camera = new FreeCamera('free-camera', new Vector3(0, 0, -20), this.scene)
         this.camera.setTarget(Vector3.Zero())
 
         this.light = new HemisphericLight('light', new Vector3(10, 10, -30), this.scene)
@@ -36,26 +43,36 @@ class App {
         this.plane = await this.loadPlane()
         this.plane.fly()
 
-        // const particle = this.createParticle()
-        // this.particles.push(particle)
         this.initParticles()
-        // ParticleHelper.CreateDefault(new Vector3(0, 0, 1)).start()
 
-        await this.main()
+        this.main()
+
     }
 
-    // 主循环函数
+    /** 主函数  */
     private async main(): Promise<void> {
-        this.scene.registerBeforeRender(() => {
-            this.updateParticles()
+        this.scene.onPointerObservable.add(info => {
+            const { event } = info
+            // 存储鼠标坐标数据
+            if (event.type === 'pointermove') {
+                this.pointerPos = {
+                    x: event.x,
+                    y: event.y
+                }
+            }
         })
 
-        // Register a render loop to repeatedly render the scene
+        this.scene.registerBeforeRender(() => {
+            this.updateParticles()
+            this.updatePlane()
+        })
+
+        // 循环
         this.engine.runRenderLoop(() => {
             this.scene.render()
         })
 
-        //resize if the screen is resized/rotated
+        // 监听浏览器缩放
         window.addEventListener('resize', () => {
             this.engine.resize()
         })
@@ -76,16 +93,16 @@ class App {
         // 新建一个透明元素 包裹模型
         const container = MeshBuilder.CreateBox('plane-container', { width: 1, depth: 2, height:1 }, this.scene)
         container.isVisible = false;
-        container.isPickable = false;
-        container.checkCollisions = true;
 
-        // container.ellipsoid = new Vector3(1, 1.5, 1);
-        // container.ellipsoidOffset = new Vector3(0, 1.5, 0);
+        // 调整到与模型重合的位置
         container.bakeTransformIntoVertices(Matrix.Translation(0, 1.2, 0.8))
         container.rotation.y = -Math.PI / 2
 
+        // 加载飞机模型
         const glb = await SceneLoader.ImportMeshAsync(null, './public/', 'plane.glb', this.scene)
         const root = glb.meshes[0]
+
+        // 绑定父子关系
         root.parent = container
 
         return {
@@ -101,6 +118,7 @@ class App {
         }
     }
 
+    /** 生成随机形状的粒子 */
     private createParticle(): Mesh {
         let mesh
         const r = Math.random()
@@ -111,44 +129,57 @@ class App {
             const height = .1 + Math.random() * .3
             const depth = .1 + Math.random() * .3
             mesh = MeshBuilder.CreateBox('box', { width, height, depth })
+            
         } else if (r < 0.66) {
             // 球
             const diameter = .1 + Math.random() * .3
             mesh = MeshBuilder.CreateSphere('sphere', { diameter })
+            
+            const material = new StandardMaterial('material', this.scene)
+            material.diffuseColor = new Color3(218 / 255, 65 / 255, 46 / 255)
+            mesh.material = material
         } else {
             // 圆锥
             const height = .1 + Math.random() * .3
             const diameter = .1 + Math.random() * .3
             mesh = MeshBuilder.CreateCylinder('cone', { height, diameter })
         }
-        const material = new StandardMaterial('material', this.scene)
-        material.diffuseColor = new Color3(1, 1, 1)
-
-        mesh.material = material
 
         return mesh
     }
 
+    /** 
+     * 生成粒子数据
+     * 每个间隔时间生成粒子：并插入到 particles 中
+     */
     private initParticles(): void {
+        // 限制 scene 最多的粒子为90
+        const LIMIT = 90
         this.particles = []
         setInterval(() => {
-            if (this.particles.length > 90) return
+            if (this.particles.length > LIMIT) return
+            // 创建粒子
             const particle = this.createParticle()
+            // 随机放置粒子
             particle.position.x = 20 + Math.random() * 20
             particle.position.y = -10 + Math.random() * 20
             particle.position.z = 0
+            // 粒子插入 particles 中：方便后面更新操作
             this.particles.push(particle)
         }, 300)
     }
 
+    /** Loop中更新粒子数据 */
     private updateParticles(): void {
+        // 定义粒子移动速度
+        const SPEED = 0.15
         this.particles.forEach((e, index) => {
+            // 粒子差不多离开屏幕后，回收重制到初始位置
             if (e.position.x < -20) {
                 e.position.x = 20 + Math.random() * 20
-                // e.dispose()
-                // this.particles.splice(index, 1)
             }
-            e.position.x -= 0.15
+            e.position.x -= SPEED
+            // 检测粒子是否和 plane 发生碰撞
             const collided = e.intersectsMesh(this.plane.mesh)
             if (collided) {
                 console.log('collided', 1)
@@ -157,6 +188,31 @@ class App {
             }
         })
     }
- 
+    
+    /** Loop中更新plane坐标 */
+    private updatePlane(): void {
+        // 设置平滑系数-不断尝试得到到数值
+        const smoothing = 2000
+        // 获取plane屏幕坐标
+        const originPos = this.WorldToScreen(this.plane.mesh.position)
+        if (this.pointerPos.x && this.pointerPos.y) {
+            // 计算鼠标位置 和 plane 位置得距离
+            const deltaX = (this.pointerPos.x - originPos.x) / smoothing
+            const deltaY = (this.pointerPos.y - originPos.y) / smoothing
+            // plane 朝鼠标的方向移动
+            this.plane.mesh.position.x += deltaX
+            this.plane.mesh.position.y -= deltaY
+        }
+    }
+
+    /** 三维世界坐标转化为屏幕坐标 */
+    public WorldToScreen(pointer: Vector3): Vector3 {
+        return Vector3.Project(
+            pointer,
+            Matrix.Identity(),
+            this.scene.getTransformMatrix(),
+            this.camera.viewport.toGlobal(this.engine.getRenderWidth(), this.engine.getRenderHeight())
+        )
+    }
 }
 new App()
